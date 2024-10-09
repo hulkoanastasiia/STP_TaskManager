420172e374a28f056d9828393558625be912b927
DarkCard1nal <34416583+DarkCard1nal@users.noreply.github.com>
2024-10-09 13:03:36 +0300

    Created entry point program.rb added constants in Constants 	new file:   constants.rb 	modified:   program.rb

---
+++ constants.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Constants
+	AUTHOR = '"Task Manager" by Shkilnyi V. and Gulko N. CS31'
+	DEFAULT_FILE_INPUT_PATH = ''
+	DEFAULT_FILE_OUTPUT_PATH = ''
+	RESULT = "The result of the selected tasks is written to a file: #{DEFAULT_FILE_OUTPUT_PATH}".freeze
+end
--- program.rb
+++ program.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+require_relative 'constants'
 require_relative 'task_list'
+# require_relative file manager
 
-puts('Hello World!')
+puts(Constants::AUTHOR)
+
+file_path = Constants::DEFAULT_FILE_INPUT_PATH
+file_path = ARGV[0] unless ARGV.empty?
+
+tasks_list # file input file_path
+
+sum_priority, result_task_list = tasks_list.generate_task_list
+
+# (sum_priority, result_task_list) output in file Constants::DEFAULT_FILE_OUTPUT_PATH if success puts(Constants::RESULT)
